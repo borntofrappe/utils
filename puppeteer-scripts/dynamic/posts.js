3e88798f8afa9184eb37119f77edc037915e8636
borntofrappe <borntofrappe@gmail.com>
2022-07-12 09:37:39 +0200

    feat: :sparkles: add template function for posts
    create a function in template.js to receive an array of articles and produce a markup document; in the body initialize an svg with the graphics and filters for all possible variants; past the svg loop through the array and create one container for each item; update the title in the h1 heading and the visual in the svg which follows; positions containers one above the other

---
+++ puppeteer-scripts/dynamic/posts.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: "Up and running",
+    datetime: "2022-07-12T8:57",
+  },
+  {
+    title: "Tangram pieces",
+    datetime: "2022-07-14T14",
+  },
+  {
+    title: "Snake",
+    datetime: "2022-07-16T20:22",
+  },
+  {
+    title: "Mots cachee",
+  },
+];
